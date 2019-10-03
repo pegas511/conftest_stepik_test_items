e86440a4522cc303a19a2c7e7a0308be59c10b55
pegas511 <pegas511@mail.ru>
2019-10-03 11:00:39 +0300

    Fixed link in test_items.py

--- test_items.py
+++ test_items.py
@@ -1,6 +1,8 @@
-link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/"
+import time
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_should_see_add_to_busket_button(browser):
     browser.get(link)
+    #time.sleep(30)
     button = browser.find_element_by_css_selector("#add_to_basket_form")
     assert button, f"There is no button {button}"
